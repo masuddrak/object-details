8ca3dfbd40454e23dfd9fa3ed649693fc33c3649
masudrana <masudranaacb@gmail.com>
2022-02-26 12:04:26 +0600

    using constructor & object create

--- object-decleration.js
+++ object-decleration.js
@@ -1,3 +1,13 @@
-// simple object
+// 1.simple object
 const student={name:'Johora Khatun',age:24}
-console.log(student)
+// console.log(student)
+
+// 2.constructor object
+const person=new Object()
+// console.log(person)
+
+// 3. new creat a object
+// const human=Object.create(null)
+const human=Object.create(student)
+
+console.log(human.age)
